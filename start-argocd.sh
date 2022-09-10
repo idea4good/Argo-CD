5998ad00d3d54b604fe5b1a2afdf0b5ff0c51164
blues-yang <blyan@microsoft.com>
2022-09-10 23:56:43 +0800

    update deployment

--- start-argocd.sh
+++ start-argocd.sh
@@ -13,13 +13,13 @@ kubectl apply -f application.yaml
 echo -e "\e[44m Login myapp(http://localhost:3200/)\e[49m"
 
 error=`kubectl port-forward -n myapp svc/myapp-service 3200:3200 2>&1`
-echo ❌$error
+echo ❌ $error
 
 while [[ $error == *"lost connection to pod"* ]]
 do
-   echo 🤝Reconnect myapp
+   echo 🤝 Reconnect myapp
    error=`kubectl port-forward -n myapp svc/myapp-service 3200:3200 2>&1`
-   echo ❌$error
+   echo ❌ $error
 done
 
 minikube stop
