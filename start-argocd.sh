696e9555c0c050e1db687a9b2b219a71c72a7bcd
blues-yang <blyan@microsoft.com>
2022-09-10 16:03:13 +0800

    change deployment and script

--- start-argocd.sh
+++ start-argocd.sh
@@ -1,13 +1,18 @@
 dockerd &
 minikube start --force --driver=docker
-kubectl apply -f application.yaml
 
-echo -e "\e[44m Login Argo CD(https://localhost:8080/) with admin/U-6jrfPOJZlCRsAL\e[49m"
+secret=`kubectl get secret argocd-initial-admin-secret -n argocd -o yaml | grep password`
+password_base64=${secret#*:}
+password=`echo $password_base64 | base64 --decode`
+
+echo -e "\e[44m Login Argo CD(https://localhost:8080/) with admin/$password\e[49m"
 kubectl port-forward -n argocd svc/argocd-server 8080:443 &
 
 echo "***************"
 
+kubectl apply -f application.yaml
 echo -e "\e[44m Login myapp(http://localhost:3200/)\e[49m"
 kubectl port-forward -n myapp svc/myapp-service 3200:3200
 
 minikube stop
+pkill dockerd
